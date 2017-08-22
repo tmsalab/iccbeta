338676d3c01528f1d4599af58da2379e33dda1ee
James Joseph Balamuta <balamut2@illinois.edu>
2017-08-21 21:56:26 -0500

    - Added Suggests field for packages used in the examples. - Improved documentation readability

--- R/datasets-docs.R
+++ R/datasets-docs.R
@@ -22,34 +22,41 @@
 #'          \code{\link{simICCdata}}
 #' @examples
 #' \dontrun{
+#' 
+#' if(requireNamespace("lme4") && requireNamespace("RLRsim")){ 
 #' data(Hofmann)
-#' require(lme4)
+#' library("lme4")
 #' 
 #' # Random-Intercepts Model
-#' lmmHofmann0 = lmer(helping ~ (1|id),data=Hofmann)
+#' lmmHofmann0 = lmer(helping ~ (1|id), data = Hofmann)
 #' vy_Hofmann = var(Hofmann[,'helping'])
 #' 
 #' # Computing icca
 #' VarCorr(lmmHofmann0)$id[1,1]/vy_Hofmann
 #' 
 #' # Estimating Group-Mean Centered Random Slopes Model, no level 2 variables
-#' lmmHofmann1  <- lmer(helping ~ mood_grp_cent + (mood_grp_cent |id),data=Hofmann,REML=F)
+#' lmmHofmann1  <- lmer(helping ~ mood_grp_cent + (mood_grp_cent |id),
+#'                      data = Hofmann, REML = FALSE)
 #' X_Hofmann = model.matrix(lmmHofmann1)
 #' P = ncol(X_Hofmann)
 #' T1_Hofmann  = VarCorr(lmmHofmann1)$id[1:P,1:P]
 #' 
 #' # Computing iccb
-#' icc_beta(X_Hofmann,Hofmann[,'id'],T1_Hofmann,vy_Hofmann)$rho_beta
+#' icc_beta(X_Hofmann, Hofmann[,'id'], T1_Hofmann, vy_Hofmann)$rho_beta
 #' 
 #' # Performing LR test
 #' # Need to install 'RLRsim' package
-#' library('RLRsim')
-#' lmmHofmann1a  <- lmer(helping ~ mood_grp_cent + (1 |id),data=Hofmann,REML=F)
-#' obs.LRT <- 2*(logLik(lmmHofmann1)-logLik(lmmHofmann1a))[1]
+#' library("RLRsim")
+#' lmmHofmann1a  <- lmer(helping ~ mood_grp_cent + (1 | id),
+#'                       data = Hofmann, REML = FALSE)
+#' obs.LRT <- 2*(logLik(lmmHofmann1) - logLik(lmmHofmann1a))[1]
 #' X <- getME(lmmHofmann1,"X")
 #' Z <- t(as.matrix(getME(lmmHofmann1,"Zt")))
 #' sim.LRT <- LRTSim(X, Z, 0, diag(ncol(Z)))
 #' (pval <- mean(sim.LRT > obs.LRT))
+#' } else {
+#'  stop("Please install packages `RLRsim` and `lme4` to run the above example.")
+#' }
 #' }
 "Hofmann"
 
@@ -81,25 +88,30 @@
 #' @examples
 #' \dontrun{
 #' data(simICCdata)
-#' require(lme4)
+#' if(requireNamespace("lme4")){ 
+#' library("lme4")
 #' 
 #' # computing icca
 #' vy <- var(simICCdata$Y)
-#' lmm0 <- lmer(Y ~ (1|l2id),data=simICCdata,REML=F)
+#' lmm0 <- lmer(Y ~ (1|l2id), data = simICCdata, REML = FALSE)
 #' VarCorr(lmm0)$l2id[1,1]/vy
 #' 
 #' # Create simICCdata2
 #' grp_means = aggregate(simICCdata[c('X1','X2')], simICCdata['l2id'],mean)
 #' colnames(grp_means)[2:3] = c('m_X1','m_X2')
-#' simICCdata2 = merge(simICCdata,grp_means,by='l2id')
+#' simICCdata2 = merge(simICCdata, grp_means, by='l2id')
 #' 
 #' # Estimating random slopes model
-#' lmm1  <- lmer(Y~I(X1-m_X1)+I(X2-m_X2) +(I(X1-m_X1)+I(X2-m_X2)|l2id),data=simICCdata2,REML=F)
+#' lmm1  <- lmer(Y ~ I(X1-m_X1) + I(X2-m_X2) + (I(X1-m_X1) + I(X2-m_X2) | l2id),
+#'               data = simICCdata2, REML = FALSE)
 #' X <- model.matrix(lmm1)
 #' p <- ncol(X)
 #' T1 <- VarCorr(lmm1) $l2id[1:p,1:p]
 #' # computing iccb
 #' # Notice '+1' because icc_beta assumes l2ids are from 1 to 30.
-#' icc_beta(X,simICCdata2$l2id+1,T1,vy)$rho_beta
+#' icc_beta(X, simICCdata2$l2id+1, T1, vy)$rho_beta
+#' } else {
+#'  stop("Please install `lme4` to run the above example.")
+#' }
 #' }
 "simICCdata"
