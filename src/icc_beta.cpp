51f5f7970e9528beaaf50da0cc6a54458c6b6f61
James J Balamuta <balamut2@illinois.edu>
2019-01-28 10:06:27 -0600

    Run clang-format on C++ file.

--- src/icc_beta.cpp
+++ src/icc_beta.cpp
@@ -107,40 +107,41 @@
 //' } 
 //' }
 // [[Rcpp::export]]
-Rcpp::List icc_beta(const arma::mat& X, const arma::vec& l2id,
-                    const arma::mat& T, double vy) {
-    
-  unsigned int N = l2id.n_elem;
-  unsigned int p = X.n_cols;
-  unsigned int J=max(l2id);
-  arma::mat SUMS = arma::zeros<arma::mat>(J,p);
-  arma::mat means = arma::zeros<arma::mat>(J,p);
-  arma::vec Nj = arma::zeros<arma::vec>(J);
-  arma::vec ONE = arma::ones<arma::vec>(N);
-  
-  for(unsigned int i=0;i<N;i++){
-    //assume l2id goes from 0 to J-1
-    Nj(l2id(i)-1) += ONE(i);
-    SUMS.row(l2id(i)-1) += X.row(i);
-  }
-  
-  for(unsigned int j=0;j<J;j++){
-    if(Nj(j)>0.0){
-      means.row(j) = SUMS.row(j)/Nj(j);
+Rcpp::List icc_beta(const arma::mat &X, const arma::vec &l2id,
+                    const arma::mat &T, double vy)
+{
+
+    unsigned int N = l2id.n_elem;
+    unsigned int p = X.n_cols;
+    unsigned int J = max(l2id);
+    arma::mat SUMS = arma::zeros<arma::mat>(J, p);
+    arma::mat means = arma::zeros<arma::mat>(J, p);
+    arma::vec Nj = arma::zeros<arma::vec>(J);
+    arma::vec ONE = arma::ones<arma::vec>(N);
+
+    for (unsigned int i = 0; i < N; ++i) {
+        // assume l2id goes from 0 to J-1
+        Nj(l2id(i) - 1) += ONE(i);
+        SUMS.row(l2id(i) - 1) += X.row(i);
     }
-  }
 
-  arma::mat XcpXc = arma::zeros<arma::mat>(p,p);
-  for(unsigned int i=0;i<N;i++){
-    XcpXc += ( X.row(i) - means.row(l2id(i)-1) ).t() * ( X.row(i) - means.row(l2id(i)-1) );
-  }
+    for (unsigned int j = 0; j < J; ++j) {
+        if (Nj(j) > 0.0) {
+            means.row(j) = SUMS.row(j) / Nj(j);
+        }
+    }
 
-  double rho_beta = trace(T * XcpXc/(sum(Nj)-1.0))/vy;  
-  
-  return Rcpp::List::create(Rcpp::Named("J",J),
-                            Rcpp::Named("means",means),
-                            Rcpp::Named("XcpXc",XcpXc),
-                            Rcpp::Named("Nj",Nj),
-                            Rcpp::Named("rho_beta",rho_beta)
-                            );
+    arma::mat XcpXc = arma::zeros<arma::mat>(p, p);
+    for (unsigned int i = 0; i < N; ++i) {
+        XcpXc += (X.row(i) - means.row(l2id(i) - 1)).t() *
+                 (X.row(i) - means.row(l2id(i) - 1));
+    }
+
+    double rho_beta = trace(T * XcpXc / (sum(Nj) - 1.0)) / vy;
+
+    return Rcpp::List::create(Rcpp::Named("J", J),
+                              Rcpp::Named("means", means),
+                              Rcpp::Named("XcpXc", XcpXc),
+                              Rcpp::Named("Nj", Nj),
+                              Rcpp::Named("rho_beta", rho_beta));
 }
